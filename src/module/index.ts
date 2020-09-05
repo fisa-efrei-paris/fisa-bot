1376446912f694c5e46616feb01cea9b96c06ae3
triozer <triozer1@gmail.com>
2020-09-05 14:18:54 +0200

    fix(github): fix module/index.ts lint

--- src/module/index.ts
+++ src/module/index.ts
@@ -2,7 +2,11 @@ import { existsSync } from "fs"
 import * as path from "path"
 
 export const loadConfiguration = (moduleName: string) => {
-  if (existsSync(path.resolve(__dirname, `../../../config/${moduleName}.local.json`))) {
+  if (
+    existsSync(
+      path.resolve(__dirname, `../../../config/${moduleName}.local.json`)
+    )
+  ) {
     return require.main?.require(`../../config/${moduleName}.local.json`)
   } else {
     return require.main?.require(`../../config/${moduleName}.json`)
